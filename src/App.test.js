04402787323dd69e0786ea3953a49f93d38b941b
Susanna Alvkvist <susannaalvkvist@hotmail.com>
2023-10-07 16:51:24 +0200

    ett test done

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+
+describe('true is Completed and false is NotCompleted', () => {
+    it('true is completed', () => {
+      expect(true).toBe(true);
+    });
+  
+    it('false is NotCompleted', () => {
+      expect(false).toBe(false);
+    });
+  });
